a079bab93971692761576c6194b979ca3c0ffba4
Matt West <workmailmattwest@gmail.com>
2023-04-15 22:11:38 +0200

    Updated Script.js
    Severely Cleaned up code.

--- script.js
+++ script.js
@@ -4,222 +4,93 @@ let playerChoice = "";
 const choices = ["rock", "paper", "scissors"];
 let comChoice = "";
 let appDOM = document.getElementById("container");
+
+// getValue() gets the current calue the player has selected in the <select> menu and stores it in a player choice var for later use
 function getValue() {
   const selectElement = document.getElementById("selectElement");
   let option = selectElement.options[selectElement.selectedIndex];
   playerChoice = option.value;
 }
-
+// retryButton() creates a button that restarts the game and clears the win/loss/tie text, as well as the "play again" button
+function retryButton(textContainer) {
+  appDOM.appendChild(textContainer);
+  const buttonDiv = document.createElement("div");
+  const retryButton = document.createElement("button");
+  const retryText = document.createTextNode("Play Again");
+  buttonDiv.classList.add("d-flex", "justify-content-center");
+  retryButton.classList.add("btn", "btn-danger");
+  appDOM.appendChild(buttonDiv);
+  buttonDiv.appendChild(retryButton);
+  retryButton.appendChild(retryText);
+  retryButton.addEventListener("click", () => {
+    textContainer.remove();
+    buttonDiv.remove();
+  });
+}
+// winText() creates the winning text
+function winText(textContainer) {
+  const textResult = document.createTextNode("You Win!");
+  textContainer.appendChild(textResult);
+  textContainer.classList.add(
+    "row",
+    "d-flex",
+    "justify-content-center",
+    "py-3"
+  );
+  retryButton(textContainer);
+}
+// lossText() creates the losing text
+function lossText(textContainer) {
+  const textResult = document.createTextNode("You Lose...");
+  textContainer.appendChild(textResult);
+  textContainer.classList.add(
+    "row",
+    "d-flex",
+    "justify-content-center",
+    "py-3"
+  );
+  retryButton(textContainer);
+}
+// tieText() creates the tie text
+function tieText(textContainer) {
+  const textResult = document.createTextNode("It's a tie");
+  textContainer.appendChild(textResult);
+  textContainer.classList.add(
+    "row",
+    "d-flex",
+    "justify-content-center",
+    "py-3"
+  );
+  retryButton(textContainer);
+}
+/* 
+checkChoice() is the logic that runs for the Rock Paper Scissors game.
+Comparing the player input and a random generated input,
+which calls the resulting text.
+*/
 function checkChoice(form) {
   getValue();
+  const textContainer = document.createElement("div");
   comChoice = choices[Math.floor(Math.random() * 3)];
   if (playerChoice == "rock" && comChoice == "scissors") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("You Win!");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    winText(textContainer);
   } else if (playerChoice == "rock" && comChoice == "paper") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("You Lose...");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    lossText(textContainer);
   } else if (playerChoice == "rock" && comChoice == "rock") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("It's a tie");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    tieText(textContainer);
   } else if (playerChoice == "paper" && comChoice == "rock") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("You Win!");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    winText(textContainer);
   } else if (playerChoice == "paper" && comChoice == "scissors") {
-    const textContainer = document.createElement("div");
     const textResult = document.createTextNode("You Lose...");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    lossText(textContainer);
   } else if (playerChoice == "paper" && comChoice == "paper") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("It's a tie");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    tieText(textContainer);
   } else if (playerChoice == "scissors" && comChoice == "paper") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("You Win!");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    winText(textContainer);
   } else if (playerChoice == "scissors" && comChoice == "rock") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("You Lose...");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    lossText(textContainer);
   } else if (playerChoice == "scissors" && comChoice == "scissors") {
-    const textContainer = document.createElement("div");
-    const textResult = document.createTextNode("It's a tie");
-    textContainer.appendChild(textResult);
-    textContainer.classList.add(
-      "row",
-      "d-flex",
-      "justify-content-center",
-      "py-3"
-    );
-    appDOM.appendChild(textContainer);
-    const buttonDiv = document.createElement("div");
-    const retryButton = document.createElement("button");
-    const retryText = document.createTextNode("Play Again");
-    buttonDiv.classList.add("d-flex", "justify-content-center");
-    retryButton.classList.add("btn", "btn-danger");
-    appDOM.appendChild(buttonDiv);
-    buttonDiv.appendChild(retryButton);
-    retryButton.appendChild(retryText);
-    retryButton.addEventListener("click", () => {
-      textContainer.remove();
-      retryButton.remove();
-    });
+    tieText(textContainer);
   }
   return false;
 }
